632697ff8deacebc2fe8b1f5ef03b2783f0f6d39
Admin <artyompodor@mail.ru>
2022-07-13 18:08:43 +0300

    Project

--- app/src/main/java/com/example/myapplication3/MainActivity.kt
+++ app/src/main/java/com/example/myapplication3/MainActivity.kt
@@ -3,17 +3,42 @@ package com.example.myapplication3
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.PopupMenu
+import android.widget.Toast
+import com.example.myapplication3.R.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button1 = findViewById<Button>(R.id.button_for_car)
-        button1.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+        val button = findViewById<Button>(R.id.button1)
+        button.setOnClickListener {
+            val popupMenu: PopupMenu = PopupMenu(this,button)
+            popupMenu.menuInflater.inflate(menu.popup_menu,popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+                when(item.itemId) {
+                    id.action_dict ->
+                        Toast.makeText(this@MainActivity, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
+                    id.action_convers ->
+                        Toast.makeText(this@MainActivity, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
+                    id.action_contacts ->
+                        Toast.makeText(this@MainActivity, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
+                }
+                true
+            })
+            popupMenu.show()
         }
+    }
+
+    fun second_activity(view: View) {
+        val second_activ=Intent(this,MainActivity2::class.java)
+        startActivity(second_activ)
+    }
+    fun third_activity(view: View){
+        val third_activ=Intent(this,MainActivity3::class.java)
+        startActivity(third_activ)
     }
 }
